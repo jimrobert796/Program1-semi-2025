8c9306c2fd25dc370f0531aae1be3f5bbc735301
jimrobert796 <jimmyroberto270@gmail.com>
2025-08-28 23:16:48 -0600

    Calculos de la media arimetica, desviacion tipica y la media armonica

--- miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.cs
+++ miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.cs
@@ -14,89 +14,90 @@ namespace miPrimerProyectoCsharp
         public Form1(){
             InitializeComponent(); // Este método es esencial para que se cargue el diseño
         }
-        private void Form1_Load(object sender, EventArgs e){
-           
-        }
-        /*
-            Monedas
-            Longitud
-            Masa
-            Volumen
-            Almacenamiento
-            Tiempo
-            Area
-         */
+        //funciones
+        //matrices
+        // LINQ -> Lenguaje de consulta integrado
 
-        String[][] etiquetas = new string[][] {
-            new string[]{"Dolar", "Pesos Mexicanos", "Quetzal", "Lempira", "Colon SV", "Cordobas", "Colon CR"}, //Monedas
-            new string[]{"Metros", "Cm", "Pulgadas","Pie", "Varas", "Yardas", "Km", "Millas"}, //Longitud
-            new string[]{"Libra", "Onza", "Gramo", "Kg", "Quintal", "Tonelada Corta"}, //Masa
-            new string[]{"Galon Us", "Litros", "Pinta Us", "Ml"}, //Volumen
-            new string[]{"GB", "Bit", "Byte", "KB", "MG", "TB"}, //Almacenamiento
-            new string[]{"Dia", "Segundos", "Minutos", "Horas", "Semana", "Meses", "Año"}, //Tiempo
-            new string[]{"Km^2","Metro", "Milla", "Yarda", "Pie", "Pulgada", "Hectaria"}, //Area 
-        };
 
-        // Matriz con los valores de conversion de uno el inical a los demas
-        // dollar a los demas y asi con los demas 
-        double[][] valores = new double[][]{
-            new double []{1,18.78, 7.66, 26.15, 8.75, 36.78, 504.12}, //Monedas
-            new double []{1, 100, 39.37, 3.28084, 1.193, 1.09361, 0.001, 0.000621371}, //Longitud
-            new double []{1, 16, 453.592, 0.453592, 0.01, 0.001,0.0005}, //Masa
-            new double []{1, 3.78541, 8, 3785.41}, //Volumen
-            new double []{1, 8e+9, 1e+9, 1e+6, 1000, 0.001}, //Almacenamiento
-            new double []{1, 86400, 1440, 24, 0.142857, 0.0328767, 0.00273973}, //Tiempo
-            new double []{1, 1e+6, 0.386102, 1.196e+6, 1.076e+7, 1.55e+9, 100 } //Tiempo
-        };
 
-        private void btnCalcular_Click(object sender, EventArgs e){
-            try
-            {
-                double cantidad = double.Parse(txtCantidadConversor.Text);
-
-                int tipo = cboTipoConversor.SelectedIndex; // Tipo de conversion
-                int de = cboDeConversor.SelectedIndex; // De que unidad
-                int a = cboAConversor.SelectedIndex; // A que unidad
-
-                // Ejemplo :  cantidad -> 12 dolares ejemplo alor[0][0] -> 18.87 / valor[0][1] -> 1
-                /// 12$ * 18.78 pesos
-                ///        ----------   =   255.36 pesos
-                ///           1$
-
-                // Funciona esta formula para cualquier conversion
-                double respuesta = cantidad * valores[tipo][a] / valores[tipo][de];
-
-                lblRespuestaConversor.Text = "RESPUESTA: " + respuesta;
-            }
-            catch(Exception er)
-            {
-                lblRespuestaConversor.Text = "ERROR"; 
-            }
-
-        }
-        // Para subirlo 
-        // Se agrego el conversor de area y validaciones
-
-        private double convertir( int tipo, int de, int a, double cantidad)
+        double media(string[] serie)
         {
-            if (cantidad <= 0)
-            {
-                return 0;
-            }
-            return cantidad * valores[tipo][a] / valores[tipo][de];
+            int n = serie.Length;
+            double suma = 0;
 
+            // entrar en cada elemento de la matriz e integrarlo en una matriz double 
+
+            // Como se calcula la media aritmética  
+            // la suma de (todos los elementos) / numero de elementos que hay 
+            // 5, 8, 4, 9 -> [5, 8, 4, 9] media = (5 + 8 + 4 + 9) / 4 = 6.5
+
+            for (int i = 0; i < n; i++)
+            {
+                suma += double.Parse(serie[i]);
+            }
+            return suma / n; // media arimetica 
         }
 
-        // Cuando cambie el cboTipoConversor
-        private void cboTipoConversor_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            cboAConversor.Items.Clear();
-            cboDeConversor.Items.Clear();
 
-            // Segun la pocicion, llenara segun el indice tomara la matriz
-            // si seleciona longitud, tomaray agregara los contenidos de la matriz de longitud en su pocicion
-            cboAConversor.Items.AddRange(etiquetas[cboTipoConversor.SelectedIndex]);
-            cboDeConversor.Items.AddRange(etiquetas[cboTipoConversor.SelectedIndex]);
+
+        double tipica (string[] serie, double m)
+        {
+
+            //Como se calcula la desviación típica
+
+            // LA SUMA DE ((cada elemento - media)^2) / numero de elementos
+            // recuerda hay que hacer una suma total de la parte de arriba para proceder a dividir
+
+            double tipica = 0;
+            double n = serie.Length;
+
+            for (int i = 0; i < serie.Length; i++)
+            {
+                int num = int.Parse(serie[i]);
+                tipica += Math.Pow(num - m, 2);
+            }
+
+            tipica = Math.Sqrt(tipica / n);
+            
+            return tipica;
+        }
+
+        double armonica(string[] serie)
+        {
+
+            // Como se calcula la media armonica
+            // seria el numero de elementos / la suma de (1 / cada elemento) <-armonica
+            // para esta hay que tener la sumatoria de 1/ cada elemento para poder dividir
+            // elementos / sumatoria = armonica 
+            double a = 0;
+            double armonica = 0;
+            int n = serie.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+                a += 1 / double.Parse(serie[i]);
+            }
+
+             armonica = n / a;
+
+            return armonica;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //int[] serie = txtxSerie.Text.Split(',').Select(n => int.Parse(n)).ToArray(); // 5, 8, 4, 9 -> [5, 8, 4, 9]
+
+            string[] serie = txtxSerie.Text.Split(','); // 5, 8, 4, 9 -> [5, 8, 4, 9]
+
+            double mediaAritmetica = media(serie); // llamo a la función media
+            double desviacionTipica = tipica(serie, mediaAritmetica); // llamo a la funcion de desviacion tipica
+            double armonicaa = armonica(serie); // llamo a la funcion de media armonica 
+
+            // mostrar los resultados en los labels correspondientes
+
+            lblMedia.Text = "Media aritmética = " + mediaAritmetica;
+            lblDesviacionTipica.Text = "Desviación típica = " + desviacionTipica;
+            lblbArmonica.Text = "Armonica = " + armonicaa;
 
         }
     }
