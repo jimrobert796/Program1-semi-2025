4653f2b0c027e929d26a3aecae2a34037e2ddf87
jimrobert796 <jimmyroberto270@gmail.com>
2025-09-11 09:06:11 -0600

    Creacion de la nueva rama academica para Computo 2 Programacion orientada a objetos y bases de datos

--- miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.Designer.cs
+++ miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.Designer.cs
@@ -29,98 +29,22 @@
         private void InitializeComponent()
         {
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.lblSerie = new System.Windows.Forms.Label();
-            this.txtxSerie = new System.Windows.Forms.TextBox();
-            this.btnCalcular = new System.Windows.Forms.Button();
-            this.lblMedia = new System.Windows.Forms.Label();
-            this.lblDesviacionTipica = new System.Windows.Forms.Label();
-            this.lblbArmonica = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // lblSerie
-            // 
-            this.lblSerie.AutoSize = true;
-            this.lblSerie.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSerie.Location = new System.Drawing.Point(46, 50);
-            this.lblSerie.Name = "lblSerie";
-            this.lblSerie.Size = new System.Drawing.Size(39, 16);
-            this.lblSerie.TabIndex = 0;
-            this.lblSerie.Text = "Serie";
-            // 
-            // txtxSerie
-            // 
-            this.txtxSerie.Location = new System.Drawing.Point(91, 46);
-            this.txtxSerie.Name = "txtxSerie";
-            this.txtxSerie.Size = new System.Drawing.Size(178, 20);
-            this.txtxSerie.TabIndex = 1;
-            // 
-            // btnCalcular
-            // 
-            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCalcular.Location = new System.Drawing.Point(91, 193);
-            this.btnCalcular.Name = "btnCalcular";
-            this.btnCalcular.Size = new System.Drawing.Size(118, 75);
-            this.btnCalcular.TabIndex = 2;
-            this.btnCalcular.Text = "Calcular";
-            this.btnCalcular.UseVisualStyleBackColor = true;
-            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
-            // 
-            // lblMedia
-            // 
-            this.lblMedia.AutoSize = true;
-            this.lblMedia.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMedia.Location = new System.Drawing.Point(46, 85);
-            this.lblMedia.Name = "lblMedia";
-            this.lblMedia.Size = new System.Drawing.Size(58, 16);
-            this.lblMedia.TabIndex = 3;
-            this.lblMedia.Text = "Media: ?";
-            // 
-            // lblDesviacionTipica
-            // 
-            this.lblDesviacionTipica.AutoSize = true;
-            this.lblDesviacionTipica.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDesviacionTipica.Location = new System.Drawing.Point(46, 119);
-            this.lblDesviacionTipica.Name = "lblDesviacionTipica";
-            this.lblDesviacionTipica.Size = new System.Drawing.Size(58, 16);
-            this.lblDesviacionTipica.TabIndex = 3;
-            this.lblDesviacionTipica.Text = "Tipica: ?";
-            // 
-            // lblbArmonica
-            // 
-            this.lblbArmonica.AutoSize = true;
-            this.lblbArmonica.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblbArmonica.Location = new System.Drawing.Point(46, 149);
-            this.lblbArmonica.Name = "lblbArmonica";
-            this.lblbArmonica.Size = new System.Drawing.Size(77, 16);
-            this.lblbArmonica.TabIndex = 3;
-            this.lblbArmonica.Text = "Armonica: ?";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(339, 290);
-            this.Controls.Add(this.lblbArmonica);
-            this.Controls.Add(this.lblDesviacionTipica);
-            this.Controls.Add(this.lblMedia);
-            this.Controls.Add(this.btnCalcular);
-            this.Controls.Add(this.txtxSerie);
-            this.Controls.Add(this.lblSerie);
             this.Name = "Form1";
-            this.Text = "ESTADISTICA";
+            this.Text = "ADMINISTRACION DE ALUMNOS";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private System.Windows.Forms.Label lblSerie;
-        private System.Windows.Forms.TextBox txtxSerie;
-        private System.Windows.Forms.Button btnCalcular;
-        private System.Windows.Forms.Label lblMedia;
-        private System.Windows.Forms.Label lblDesviacionTipica;
-        private System.Windows.Forms.Label lblbArmonica;
     }
 }
 
--- miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.cs
+++ miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.cs
@@ -14,69 +14,9 @@ namespace miPrimerProyectoCsharp
         public Form1(){
             InitializeComponent(); // Este método es esencial para que se cargue el diseño
         }
-        //funciones
-        //matrices
-        // LINQ -> Lenguaje de consulta integrado
-
-
-
-        double media(int[] serie)
+        
+        private void Form1_Load(object sender, EventArgs e)
         {
-
-            // entrar en cada elemento de la matriz e integrarlo en una matriz double 
-
-            // Como se calcula la media aritmética  
-            // la suma de (todos los elementos) / numero de elementos que hay 
-            // 5, 8, 4, 9 -> [5, 8, 4, 9] media = (5 + 8 + 4 + 9) / 4 = 6.5
-
-            
-            return serie.Average(); // función de LINQ quec calcula la media aritmética
-        }
-
-
-
-        double tipica (int[] serie, double m)
-        {
-
-            //Como se calcula la desviación típica
-
-            // LA SUMA DE ((cada elemento - media)^2) / numero de elementos
-            // recuerda hay que hacer una suma total de la parte de arriba para proceder a dividir
-
-            
-            return Math.Sqrt(serie.Average(n=>Math.Pow(n -m, 2))); // función de LINQ que calcula la desviación típica
-            // Voy a ser sincero me cuesta entenderlo es como lamba ... 
-        }
-
-        double armonica(int[] serie)
-        {
-
-            // Como se calcula la media armonica
-            // seria el numero de elementos / la suma de (1 / cada elemento) <-armonica
-            // para esta hay que tener la sumatoria de 1/ cada elemento para poder dividir
-            // elementos / sumatoria = armonica 
-            
-            int n = serie.Length;
-
-
-            return n / serie.Sum(num => 1.0/num); // funciona pero es mas legible ya comprendi
-        }
-
-        private void btnCalcular_Click(object sender, EventArgs e)
-        {
-            int[] serie = txtxSerie.Text.Split(',').Select(n => int.Parse(n)).ToArray(); // 5, 8, 4, 9 -> [5, 8, 4, 9]
-
-           
-
-            double mediaAritmetica = media(serie); // llamo a la función media
-            double desviacionTipica = tipica(serie, mediaAritmetica); // llamo a la funcion de desviacion tipica
-            double armonicaa = armonica(serie); // llamo a la funcion de media armonica 
-
-            // mostrar los resultados en los labels correspondientes
-
-            lblMedia.Text = "Media aritmética = " + mediaAritmetica;
-            lblDesviacionTipica.Text = "Desviación típica = " + desviacionTipica;
-            lblbArmonica.Text = "Armonica = " + armonicaa;
 
         }
     }
